d74786218c26fc799d1519b4b4a6b74bd16ddd14
Jawad Sefiani <jawad.sefiani@hotmail.com>
2020-04-01 22:28:57 +0200

    migrations: initial migration

---
+++ Webshop.DataAccess/Migrations/20200401195234_InitialMigration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Webshop.DataAccess.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: true),
+                    Category = table.Column<string>(nullable: true),
+                    Price = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Stock = table.Column<int>(nullable: false),
+                    SKU = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Description", "Name", "Price", "SKU", "Stock" },
+                values: new object[] { "1", "Jackets", "This is a description", "VIRGIL TRUCKER - Spijkerjas", 2799, "1589420", 1 });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Category", "Description", "Name", "Price", "SKU", "Stock" },
+                values: new object[] { "2", "Trousers", "This is a description", "ASOS DESIGN smart tapered trousers in black", 3499, "1572163", 10 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
