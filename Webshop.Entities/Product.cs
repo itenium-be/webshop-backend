cc27f05edd34f42a277fbac397d8b8f1b05becc5
Wouter Van Schandevijl <woutervs@hotmail.com>
2020-04-04 14:31:31 +0200

    Need to use decimals for money values

--- Webshop.Entities/Product.cs
+++ Webshop.Entities/Product.cs
@@ -9,7 +9,7 @@ namespace Webshop.Entities
         public string Id { get; set; }
         public string Name { get; set; }
         public string Category { get; set; }
-        public int Price { get; set; }
+        public decimal Price { get; set; }
         public string Description { get; set; }
         public int Stock { get; set; }
         public string SKU { get; set; }
